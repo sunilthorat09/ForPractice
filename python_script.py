007ab74dc38f1605c13dde40bfb200e51bf75198
sunilthorat09 <sunilthorat09@gmail.com>
2015-09-21 11:53:56 +0530

    small correction in syntax

--- python_script.py
+++ python_script.py
@@ -3,11 +3,12 @@
 import csv
 
 fp = open("power.csv", "r+")
-fp.write("Sunil,Thorat,20")
-fp.write("Chetan,Jain,23")
+#fp.write("Sunil,Thorat,20\n")
+#fp.write("Chetan,Jain,23")
 
 fileContent = csv.reader(fp)
-print(fileContent)
 
+for row in fileContent:
+	print(row)
 
 fp.close()
